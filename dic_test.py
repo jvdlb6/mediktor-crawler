91ed523483d9422b2069086083d9bbd824b50a99
João Victor Dias <jvdlb6@gmail.com>
2022-08-30 00:14:24 -0300

    atualizando json

---
+++ dic_test.py
@@ -0,0 +1,58 @@
+import unicodedata
+import time
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import json
+
+
+url = "https://www.mediktor.com/pt-br/doenca/ataxia-cerebelar-aguda-pos-infecciosa-pediatria?conclusionId=958d85bf-e96a-4487-ab63-5772a6204661"
+option = Options()
+option.headless = True
+driver = webdriver.Chrome(options=option)
+driver.get(url)
+time.sleep(10)
+
+
+def normalizeString(string: str):
+    normalized = unicodedata.normalize('NFD', string)
+    return normalized.encode('ascii', 'ignore').decode('utf-8')
+
+
+doencas = []
+
+nome = driver.find_element(By.XPATH,
+                           "//div[@class='mdk-conclusion-detail__main-title']"
+                           ).text
+desc = driver.find_element(By.XPATH,
+                           "//div[@class='mdk-conclusion-detail__main-description']"
+                           ).text.splitlines()
+
+epidem = driver.find_element(By.XPATH,
+                             "//div[@class='mdk-ui-card__content']"
+                             ).text.splitlines()
+
+sintomas = driver.find_element(By.XPATH,
+                               "//body[1]/div[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[1]/div[3]/div[2]/div[2]/div[2]"
+                               ).text.splitlines()
+
+fat_relac = driver.find_element(By.XPATH,
+                                "(//div[@data-qa-ta='cardContentEl'])[3]"
+                                ).text.splitlines()
+
+espec = driver.find_element(By.XPATH,
+                            "(//div[@class='mdk-ui-card__content'])[4]"
+                            ).text.splitlines()
+doencas.append(f"Nome: {normalizeString(str(nome))}, {normalizeString(str('Descrição:'))} {normalizeString(str(desc))}\
+                       ,Epidemiologia: {normalizeString(str(epidem))},Sintomas: {normalizeString(str(sintomas))},\
+                       Fatores Relacionados: {normalizeString(str(fat_relac))}, {normalizeString(str('Especialização:'))}, {normalizeString(str(espec))}")
+
+# criando o arquivo json
+json_object = json.dumps(doencas, indent=5)
+with open("doencas.json", "w") as outfile:
+    outfile.write(json_object)
+
+df = pd.DataFrame(doencas, index=[' '])
+df.to_csv('doencas.csv', sep='\t', encoding='utf-8')
+driver.quit()
---
+++ dicionario.py
@@ -0,0 +1,124 @@
+import time
+import unicodedata
+import pandas as pd
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import json
+
+url = "https://www.mediktor.com/pt-br/glossario"
+option = Options()
+option.headless = True
+driver = webdriver.Chrome(options=option)
+driver.get(url)
+time.sleep(10)
+
+# Caminho para os links
+el_links = driver.find_elements(
+    By.XPATH, "//a[@class='mdk-dictionary-list__glossary-item']")
+
+# declarando dicionarios
+urls = []
+nome_doenca = []
+desc = []
+epidem = []
+sintomas = []
+fat = []
+espec = []
+doenca_info = []
+
+
+# func para remover ç e acentos
+
+def normalizeString(string: str):
+    normalized = unicodedata.normalize('NFD', string)
+    return normalized.encode('ascii', 'ignore').decode('utf-8')
+
+
+# capturando as urls e guardando no dicionario urls #
+for i in range(len(el_links)):
+    urls.append(el_links[i].get_attribute('href'))
+
+    # seguindo cada url dentro do dicionario e
+    # crawleando as informações necessárias
+for link in urls:
+
+    driver.get(link)
+    print(link)
+    waitReturn = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH,
+                                        "//div[@class='mdk-conclusion-detail__main-title']"
+                                        )))
+    nome_source = driver.find_element(By.XPATH,
+                                      "//div[@class='mdk-conclusion-detail__main-title']"
+                                      ).text
+    try:
+        epidem_source = driver.find_element(By.XPATH,
+                                            "//div[@class='mdk-ui-card__content']"
+                                            ).text.splitlines()
+    except NoSuchElementException:
+        epidem.append('Nao possui Epidemiologia.')
+        pass
+    try:
+        sintomas_source = driver.find_element(By.XPATH,
+                                              "//body[1]/div[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[1]/div[3]/div[2]/div[2]/div[2]"
+                                              ).text.splitlines()
+    except NoSuchElementException:
+        sintomas.append('Nao possui Sintomas.')
+        pass
+
+    try:
+        fat_source = driver.find_element(By.XPATH,
+                                         "(//div[@data-qa-ta='cardContentEl'])[3]"
+                                         ).text.splitlines()
+    except NoSuchElementException:
+        fat.append('Nao possui Fatores Relacionados.')
+        pass
+
+    try:
+        espec_source = driver.find_element(By.XPATH,
+                                           "(//div[@class='mdk-ui-card__content'])[4]"
+                                           ).text.splitlines()
+    except NoSuchElementException:
+        espec.append('Nao possui Especialidades.')
+        pass
+
+    try:
+        desc_source = driver.find_element(By.XPATH,
+                                          "//div[@class='mdk-conclusion-detail__main-description']"
+                                          ).text.splitlines()
+    except NoSuchElementException:
+        desc.append(normalizeString(str('Nao possui Descricao.')))
+        pass
+
+    # povoando os arrays com a função para remover ascii
+
+    doenca_info.append(f"Nome: {normalizeString(str(nome_source))}, Epidemiologia: {normalizeString(str(epidem_source))}, Descricao: {normalizeString(str(desc_source))},Sintomas: {normalizeString(str(sintomas_source))},{normalizeString(str(fat_source))}, Especializacao:, {normalizeString(str(espec_source))}")
+    # desc.append(normalizeString(str(desc_source)))
+    # epidem.append(normalizeString(str(epidem_source)))
+    # sintomas.append(normalizeString(str(sintomas_source)))
+    # nome_doenca.append(normalizeString(str(nome_source)))
+    # fat.append(normalizeString(str(fat_source)))
+    # espec.append(normalizeString(str(espec_source)))
+    # povoando o dicionario
+
+    # doenca_info['url'] = url
+    # doenca_info['Nome'] = nome_doenca
+    # doenca_info['Descricao'] = desc
+    # doenca_info['Epidemiologia'] = epidem
+    # doenca_info['Sintomas'] = sintomas
+    # doenca_info['Fatores relacionados'] = fat
+    # doenca_info['Especialidades'] = espec
+
+    driver.back()
+# criando arquivo json
+
+json_object = json.dumps(doenca_info, indent=5)
+with open("doencas.json", "w") as outfile:
+    outfile.write(json_object)
+df = pd.DataFrame(doenca_info, index=[' '])
+df.to_csv('doencas.csv', sep='\t', encoding='utf-8')
+driver.quit()
